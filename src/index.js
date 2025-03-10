5459b6164a42aac68bcf6b6c4d45c025415717c4
DharmeshV05 <dharmeshbm1@gmail.com>
2025-03-10 16:33:25 +0530

    Revert "first commit"
    This reverts commit 747e13e6b9cdbd9ae80a18d2f7ee91e0d198b17b.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import App from "./App"; // Assuming App.js is the entry point
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </GoogleOAuthProvider>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
